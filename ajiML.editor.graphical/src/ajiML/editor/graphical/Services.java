a0b212e38db2073f9e580081b0270a21649ea401
Jonas <jonas.sorgalla@fh-dortmund.de>
2018-05-08 14:44:00 +0200

    added automatical entity import (copy by value) to a services' domain model when another service is consumed  (see #7)

--- ajiML.editor.graphical/src/ajiML/editor/graphical/Services.java
+++ ajiML.editor.graphical/src/ajiML/editor/graphical/Services.java
@@ -1,17 +1,41 @@
 package ajiML.editor.graphical;
 
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import ajiML.Ability;
+import ajiML.DataModel;
+import ajiML.Entity;
+import ajiMLT.CustomT;
+import ajiMLT.EntityT;
+import ajiMLT.ServiceInterfaceT;
 
 /**
  * The services class used by VSM.
  */
 public class Services {
-    
+	//Default Constructor
+    public Services() {
+    	
+    }
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
-    public EObject myService(EObject self, String arg) {
-       // TODO Auto-generated code
-      return self;
+    public EList<Entity> addInterfaceSubjects(EObject self, ServiceInterfaceT arg) {
+     EList<Entity> returnValue = ((DataModel) self).getEntities();
+     for (Ability a : arg.getAbilities()) {
+    	if(a instanceof CustomT) {
+    		  //nothing to add for custom abilities
+    	}	else {
+    		EntityT subject = (EntityT) a.eGet(a.eClass().getEStructuralFeature("subject"));
+    		if(!returnValue.contains(subject)) {
+    			returnValue.add(subject);
+    		}
+    	  }     	  
+      }
+      return returnValue;
     }
 }
